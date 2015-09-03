417a53416cb2b5ac0b0d1b2e1921c7c6d1bb1e5a
GregOSF <gregory.onza@gmail.com>
2015-09-03 12:03:34 -0700

    initial user schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150903171527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "city"
+    t.string   "meeting_place"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
