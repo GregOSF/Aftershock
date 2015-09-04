feb87bef89f2893a649e68f0ca2cffda68468788
GregOSF <gregory.onza@gmail.com>
2015-09-04 09:55:51 -0700

    Add pre/post quake tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903171527) do
+ActiveRecord::Schema.define(version: 20150903215124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "postquake_todos", force: :cascade do |t|
+    t.string   "todo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "prequake_todos", force: :cascade do |t|
+    t.string   "todo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
