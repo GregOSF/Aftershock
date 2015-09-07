0fde8f4c2776fd8f20619eac8f7602c32ebb01b4
GregOSF <gregory.onza@gmail.com>
2015-09-07 12:47:06 -0700

    Add CRUD methods

--- app/controllers/prequake_controller.rb
+++ app/controllers/prequake_controller.rb
@@ -1,25 +1,62 @@
 class PrequakeController < ApplicationController
 
   def index
-    @prequake_todos = Todo.where("event = 'Prequake'")
+    @prequakeseed_todos = Preseed.where("event = 'Prequake'")
     render :index    
   end
 
   def new
+    @prequake_todo = Todo.new
+    render :new
   end
 
+  
   def create
+    if current_user
+      @prequake_todo = Todo.create(prequake_params)
+      redirect_to prequakelist_path
+    else
+      redirect_to login_path
+    end
   end
 
   def show
+
   end
 
   def edit
+    @user = current_user
+    @prequake_todo = Todo.find(params[:id])
+    if current_user.todos.include? @todo
+      render :edit
+    else 
+      session[:user_id] = nil
+      redirect_to profile_path
+    end
   end
 
   def update
+    @user = current_user
+    prequake_todo = Todo.find(params[:id])
+    if current_user.todos.include? story
+      todo.update_attributes(prequake_params)
+      redirect_to prequake_index_path
+    else
+      redirect_to profile_path
+    end
   end
 
   def destroy
+    @user = current_user
+    prequake_todo = Todo.find(params[:id])
+    prequake_todo.destroy
+    redirect_to profile_path
+    
   end
+
+
+  private
+    def prequake_params
+      params.require(:todo).permit(:desc, :event)
+    end
 end
