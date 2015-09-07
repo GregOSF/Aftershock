d8a113223ab73501a2159a87fbc174d9da8e87d9
GregOSF <gregory.onza@gmail.com>
2015-09-07 12:51:22 -0700

    add preseed table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904180832) do
+ActiveRecord::Schema.define(version: 20150906223042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "preseeds", force: :cascade do |t|
+    t.text     "desc"
+    t.string   "event"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.text     "desc"
