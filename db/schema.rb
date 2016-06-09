50b64411d1b64ddc1838348243c90958e4799ac9
GregOSF <gregory.onza@gmail.com>
2016-06-08 22:38:03 -0700

    reset database. i've made a terrible mistake.....

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906223042) do
+ActiveRecord::Schema.define(version: 20150911153240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20150906223042) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "contact_num"
+    t.string   "contact_name"
   end
 
 end
